962e4e08008f0959e9c2fecda1947b3d88b99647
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-10-16 13:07:18 -0400

    feature layout finished

--- pages/index.js
+++ pages/index.js
@@ -10,25 +10,62 @@ export default function Home() {
 
       <div className="feature-container">
         <div className="item">
-          <header>
-            <h1>Hi - WAIST</h1>
-          </header>
+          <div className="vertical-aligner-helper">
+            <div className="product-copy">
+              <p>
+                <span>Hi - WAIST</span>
+              </p>
+
+              <p>
+                Heavy days. Holds up to 2 tampon's worth.com
+                <br />
+                <br />
+                The phrase "treat yo'self" reaches new belly-button brushing
+                heights with the HiWaist, and it's here just in time for cuffung
+                season.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="item item--order-modifier">
           <img
             src="https://cdn.accentuate.io/8488635591/11611289059400/10-hi-waist-grapefruit-v1596554281555.png?700x1000"
             alt=""
           />
         </div>
         <div className="item">
-          <img
-            src="https://cdn.accentuate.io/8488635591/11611289059400/10-hi-waist-grapefruit-v1596554281555.png?700x1000"
-            alt=""
-          />
-        </div>
-        <div className="item">
-          <img
-            src="https://cdn.accentuate.io/8488635591/11611289059400/10-hi-waist-grapefruit-v1596554281555.png?700x1000"
-            alt=""
-          />
+          <div className="vertical-aligner-helper">
+            <div>
+              <form>
+                <p>
+                  <span>$35.00</span>
+                </p>
+
+                <ul id="menu">
+                  <li>color</li>
+                  <li>black</li>
+                  <li>beige</li>
+                </ul>
+                <select name="sizes" id="sizes">
+                  <option value="0">size</option>
+                  <option value="xx-small">xx-small</option>
+                  <option value="x-small">x-small</option>
+                  <option value="small">small</option>
+                  <option value="medium">medium</option>
+                  <option value="large">large</option>
+                  <option value="x-large">x-large</option>
+                </select>
+                <label for="sizes">
+                  {' '}
+                  <a href="#">what's my size</a>
+                </label>
+                <br />
+                <br />
+                <button>add to cart</button>
+                <a href="#">build a cycle set and save up to 20%</a>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
       <div className="gallery-container">
